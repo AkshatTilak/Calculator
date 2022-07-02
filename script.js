d7489f117a1ea28e91e8e30d06057ac4494515ec
Akshat Tilak <akshattilak@gmail.com>
2022-07-02 15:57:12 +0530

    added functionality for the =, clear

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // constants and variables
 const buttons = document.getElementById("buttons");
-console.log(buttons);
+var currDisplay = "";
+var currOp = "";
+var values = [];
+var ind = 0;
+var equalsHappend = false;
 
 for( var i = 0 ; i < buttons.children.length; i++){
     buttons.children[i].addEventListener("click", findAction);
@@ -9,34 +13,122 @@ for( var i = 0 ; i < buttons.children.length; i++){
 
 
 
-// add, mult, sub, div, remainder, +/- functions
-function add(val1 , val2){return val1 + val2;} 
-function mult(val1 , val2){return val1 * val2;}
+//functions
+function add(val1 , val2){return String(val1 + val2);} 
+function sub(val1 , val2){return String(val1 - val2);} 
+function mult(val1 , val2){return String(val1 * val2);}
 function div(val1, val2){
     if (val2 == 0) {return "80085";}
     result = Number((val1 / val2).toFixed(3));
-    return result;
+    return String(result);
 }
 function remainder(val1, val2){
     if (val2 == 0) {return "Its INFINITE"}
-    return val1 % val2;
+    return String(val1 % val2);
 }
 function changeSign(val){
-    return -val;
+    return String(-val);
 }
 
 function findAction(e){
+    if(equalsHappend){
+        clear();
+        equalsHappend = false;
+    }
     var currClass = e.target.className; 
     if(currClass === "num"){
-        console.log("this is a number");
-        dispNum();
+        dispNum(e);
     }
     else if(currClass === "op"){
-        console.log("this is a operator");
-        dispOp();
+        dispOp(e);
     }
     else{
-        console.log("this is the rest");
-        dispRest();
+        dispRest(e);
     }
+}
+
+function dispNum(e){
+    currDisplay += e.target.value;
+}
+
+function dispOp(e){
+    
+    if (currDisplay !== ""){
+        values[ind] = currDisplay;
+        ind++;
+        if(values.length === 2){
+            operate();
+        }
+        currDisplay = "";
+        currOp = e.target.value;
+        console.log(currOp, values, values.length, currDisplay);
+    }
+    else{
+        currOp = e.target.value;
+        console.log(currOp, values, values.length, currDisplay);
+    }
+}
+
+function dispRest(e){
+    if (e.target.className === "clear"){
+        clear();
+    }
+    else if( e.target.className === "dot"){
+
+    }
+    else if( e.target.className === "equals"){
+            equals();
+            equalsHappend = true;
+    }
+    console.log(currOp, values, values.length, currDisplay);
+}
+
+function clear(){
+    currDisplay = "";
+    currOp = "";
+    values = [];
+    ind = 0;
+}
+
+function dot(){
+
+}
+
+function equals(){
+    if(values.length === 1 && currDisplay !== ""){
+        values[ind] = currDisplay;
+        ind++;
+        operate();
+        currDisplay = values[0];
+        values = [];
+        ind = 0;
+    }    
+}
+function operate(){
+    var res = "";
+    console.log(Number(values[0]), Number(values[1]));
+    switch (currOp){
+        case "+":
+            res = add(Number(values[0]), Number(values[1]));
+            break;
+        case "-":
+            res = sub(Number(values[0]), Number(values[1]));
+            break;
+        case "/":
+            res = div(Number(values[0]), Number(values[1]));
+            break;
+        case "%":
+            res = remainder(Number(values[0]), Number(values[1]));
+            break;
+        case "*":
+            res = mult(Number(values[0]), Number(values[1]));
+            break;
+        default:
+            console.log('wtf');
+    }
+    values = [];
+    ind = 0;
+    values[ind] = res;
+    ind++;
+    currDisplay = "";
 }
